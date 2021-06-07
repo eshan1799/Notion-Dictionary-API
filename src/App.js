3a9f11ad066ff70bdc33018fba541537ea3c71d1
eshan1799 <eshan1799@gmail.com>
2021-06-08 00:12:50 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import Main from './Components/main'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Main></Main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/main.js
@@ -0,0 +1,14 @@
+function Main() {
+    return (
+        <div id="main">
+            <form>
+                <input type="text" autoFocus></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="submit"></input>
+            </form>
+        </div>
+    )
+}
+
+export default Main;
