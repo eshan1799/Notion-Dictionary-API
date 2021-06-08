a9c967604f8489a2194402d5ea8ab61412202114
eshan1799 <eshan1799@gmail.com>
2021-06-08 01:25:52 +0100

    initial setup

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,14 +1,22 @@
 function Main() {
-    return (
-        <div id="main">
-            <form>
-                <input type="text" autoFocus></input>
-                <input type="text"></input>
-                <input type="text"></input>
-                <input type="submit"></input>
-            </form>
-        </div>
-    )
+    function handleChange(e) {
+        console.log(e.target.value)
+    }
+    function checkDictionary(){
+        console.log("check")
+    }
+  return (
+    <form>
+      <label htmlFor="word">Word</label>
+      <input id="word" onChange={ handleChange } type="text" autoFocus></input>
+      <input id="checkButton" onClick={ checkDictionary } value="check" type="button"></input>
+      <label htmlFor="definition">Definition</label>
+      <input id="definition" type="text"></input>
+      <label htmlFor="synonyms">Synonyms</label>
+      <input id="synonyms" type="text"></input>
+      <input type="submit"></input>
+    </form>
+  );
 }
 
 export default Main;
