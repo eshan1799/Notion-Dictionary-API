d6bc9676dfa3893f142357f3942cb42006a022e1
eshan1799 <eshan1799@gmail.com>
2021-06-08 16:35:29 +0100

    add synonym data to textarea

--- src/Components/main.js
+++ src/Components/main.js
@@ -8,15 +8,34 @@ function Main() {
     function handleChange(e) {
         setWord(word = e.target.value)
     }
+
     function checkDictionary(){
       fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${ word }`)
       .then(response => response.json())
-      // .then(data => console.log(data[0].meanings[0].definitions[0].definition))
-      .then(data => setDefinition(definition = data[0].meanings[0].definitions[0].definition))
-      .then(console.log(definition, synonyms));
+      .then(data => changeState(data))
+      .catch((error) => {
+        console.error('Error:', error)
+      })
     }
+
+  function changeState(data) {
+    setDefinition(definition = data[0].meanings[0].definitions[0].definition)
+    setSynonyms(data[0].meanings[0].definitions[0].synonyms)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // fetch(clientConfiguration['filesApi.local'], {
+    //   method: 'POST',
+    //   headers: new Headers(),
+    //   body: JSON.stringify({ communityName: communityName, body: files })
+    //     }).then((res) => res.json())
+    //   .then((data) => console.log(data))
+    //   .catch((err) => console.log(err))
+  }
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <label htmlFor="word">Word</label>
       <input id="word" onChange={ handleChange } type="text" autoFocus></input>
       <input id="checkButton" onClick={ checkDictionary } value="check" type="button"></input>
