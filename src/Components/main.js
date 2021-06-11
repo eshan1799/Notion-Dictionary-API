d1eff401358db87e3b7bfde6ed4d950d4a147a04
eshan1799 <eshan1799@gmail.com>
2021-06-11 14:59:31 +0100

    add notion sdk code

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+// import Form from './form';
+
+const { Client } = require('@notionhq/client');
 
 function Main() {
   let [word, setWord] = useState('');
@@ -34,6 +37,73 @@ function Main() {
     //   .catch((err) => console.log(err))
   }
 
+  const notion = new Client({
+    auth: process.env.NOTION_API_TOKEN,
+  })
+
+  const newPage = (async () => {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: '7f205ba4dc104e8f8d47ef96c80eb572?v=342592dcdd9e48a699ddf0faebfe4f96',
+      },
+      properties: {
+        Word: {
+          title: [
+            {
+              text: {
+                content: word,
+              },
+            },
+          ],
+        },
+        Definition: {
+          text: {
+            content: definition,
+          },
+        },
+        Synonym: {
+          text: {
+            content: synonyms,
+          },
+        },
+      }
+      // children: [
+      //   {
+      //     object: 'block',
+      //     type: 'heading_2',
+      //     heading_2: {
+      //       text: [
+      //         {
+      //           type: 'text',
+      //           text: {
+      //             content: 'Lacinato kale',
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   },
+      //   {
+      //     object: 'block',
+      //     type: 'paragraph',
+      //     paragraph: {
+      //       text: [
+      //         {
+      //           type: 'text',
+      //           text: {
+      //             content: 'Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.',
+      //             link: {
+      //               url: 'https://en.wikipedia.org/wiki/Lacinato_kale',
+      //             },
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   },
+      // ],
+    });
+    console.log(response);
+  })();
+
   return (
     <form onSubmit={ handleSubmit }>
       <label htmlFor="word">Word</label>
@@ -45,6 +115,7 @@ function Main() {
       <textarea id="synonyms" value={ synonyms } readOnly></textarea>
       <input type="submit"></input>
     </form>
+    // <Form props={ definition, synonyms, handleChange, handleSubmit, checkDictionary }></Form>
   );
 }
 
