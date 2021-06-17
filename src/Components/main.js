1d871ad9ed557151962100564aedf308cdcd95f7
eshan1799 <eshan1799@gmail.com>
2021-06-17 18:45:48 +0100

    begin structure for POST request

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-// import Form from './form';
-
-const { Client } = require('@notionhq/client');
 
 function Main() {
   let [word, setWord] = useState('');
@@ -28,81 +25,27 @@ function Main() {
 
   function handleSubmit(e) {
     e.preventDefault()
-    // fetch(clientConfiguration['filesApi.local'], {
-    //   method: 'POST',
-    //   headers: new Headers(),
-    //   body: JSON.stringify({ communityName: communityName, body: files })
-    //     }).then((res) => res.json())
-    //   .then((data) => console.log(data))
-    //   .catch((err) => console.log(err))
+    const formattedData = {
+      postWord: `${ word }`,
+      postDefinition: `${ definition }`,
+      postSynonyms: `${ synonyms }`
+    }
+    postData('http://localhost:5000/', { postWord: `${ word }` })
+    .then(data => {
+      console.log(data)
+    });
   }
 
-  const notion = new Client({
-    auth: process.env.NOTION_API_TOKEN,
-  })
-
-  const newPage = (async () => {
-    const response = await notion.pages.create({
-      parent: {
-        database_id: '7f205ba4dc104e8f8d47ef96c80eb572?v=342592dcdd9e48a699ddf0faebfe4f96',
+  async function postData(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
       },
-      properties: {
-        Word: {
-          title: [
-            {
-              text: {
-                content: word,
-              },
-            },
-          ],
-        },
-        Definition: {
-          text: {
-            content: definition,
-          },
-        },
-        Synonym: {
-          text: {
-            content: synonyms,
-          },
-        },
-      }
-      // children: [
-      //   {
-      //     object: 'block',
-      //     type: 'heading_2',
-      //     heading_2: {
-      //       text: [
-      //         {
-      //           type: 'text',
-      //           text: {
-      //             content: 'Lacinato kale',
-      //           },
-      //         },
-      //       ],
-      //     },
-      //   },
-      //   {
-      //     object: 'block',
-      //     type: 'paragraph',
-      //     paragraph: {
-      //       text: [
-      //         {
-      //           type: 'text',
-      //           text: {
-      //             content: 'Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.',
-      //             link: {
-      //               url: 'https://en.wikipedia.org/wiki/Lacinato_kale',
-      //             },
-      //           },
-      //         },
-      //       ],
-      //     },
-      //   },
-      // ],
+      body: data
     });
-    console.log(response);
-  })();
+    return response.json();
+  }
 
   return (
     <form onSubmit={ handleSubmit }>
@@ -115,7 +58,6 @@ function Main() {
       <textarea id="synonyms" value={ synonyms } readOnly></textarea>
       <input type="submit"></input>
     </form>
-    // <Form props={ definition, synonyms, handleChange, handleSubmit, checkDictionary }></Form>
   );
 }
 
