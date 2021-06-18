18683f60d6ca03718ccb208599eb5d95512af0e5
eshan1799 <eshan1799@gmail.com>
2021-06-18 14:37:38 +0100

    file restructuring to provide an independant client & server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+import Main from "./Components/main";
+// require('dotenv').config()
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Main></Main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
